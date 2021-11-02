4f6cac5fd270e43b87a3609b885b534a742ccd17
Rodrigo - Xita rps <xitarps@gmail.com>
2021-11-01 21:56:49 -0300

    [Refactor/Fix]: Refactor code to implement comments within news

---
+++ app/controllers/news/comments_controller.rb
@@ -0,0 +1,35 @@
+class News::CommentsController < ApplicationController
+  # before_action :find_comment, only: %i[index new]
+  before_action :find_single_News, only: %i[index create]
+
+  def index
+    @comments = Comment.all.order(updated_at: :desc)
+    @current_user_comment = Comment.new
+  end
+
+  def create
+
+    news_data = { news: @single_news }
+    @current_user_comment = current_user.comments
+                                        .new(comment_paramn.merge news_data )
+
+    if @current_user_comment.save 
+      msg = 'Comentario adicionado com sucesso'
+      redirect_to comments_index_path({id: @single_news.slug}), notice: msg
+    end 
+  end
+
+  # def new
+  #   @current_user_comment = Comment.new
+  # end
+
+  private
+
+  def find_single_News
+    @single_news = News.find_by(slug: params[:id])
+  end
+
+  def comment_paramn
+    params.require(:comment).permit(:content)
+  end
+end
